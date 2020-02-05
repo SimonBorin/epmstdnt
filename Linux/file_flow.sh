630ffbc3dfcb6ffce0206ea555cc0e60b5ccd596
Semen_Borin <semen_borin@epam.com>
2020-02-05 15:12:17 +0300

    uuid check + logs

--- Linux/file_flow.sh
+++ Linux/file_flow.sh
@@ -43,12 +43,56 @@ function _read_config() {
 }
 
 #===  FUNCTION  ================================================================
+#         NAME:  _mv
+#  DESCRIPTION:  moves and renames files from src to dst and log it
+#===============================================================================
+
+function _mv() {
+    echo $1
+    CUR_DATE=$(date +"%Y-%m-%d %H:%M:%S")
+    OBJ_NAME=$(awk 'BEGIN{FS="\t"} NR==1 {print $1}' "$1")
+    OBJ_DATE=$(awk 'BEGIN{FS="\t"} NR==1 {print $2}' "$1")
+    OBJ_UUID=$(awk 'END{if (/^[0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12}/) print $1}' second.txt)
+    OBJ_NEW_NAME="$OBJ_NAME"_"$OBJ_DATE"_"$CUR_DATE"
+    if [ ! -z "$OBJ_UUID" ]; then
+        mv $1 "$TARGETDIR"/"$OBJ_NEW_NAME"
+        echo "$1 moved to $TARGETDIR and renamed to $OBJ_NEW_NAME" >> $LOGFILENAME
+    else
+        echo "File $1 is not redy! UUID wasn't detected" >> $LOGFILENAME
+    fi 
+    
+}
+
+#===  FUNCTION  ================================================================
 #         NAME:  main
 #  DESCRIPTION:  main func
 #===============================================================================
 
 function main() {
     _read_config
+    touch $LOGFILENAME 
+    echo date >> $LOGFILENAME
+    # cur_date=$(date +"%Y-%m-%d %H:%M:%S")
+    # declare $(awk 'BEGIN{OFS="\t"} NR==1 {print OBJ_NAME=$1, OBJ_DATE=$2}' second.txt)
+    # OBJ_NAME=$(awk 'BEGIN{FS="\t"} NR==1 {print $1}' second.txt)
+    # OBJ_DATE=$(awk 'BEGIN{FS="\t"} NR==1 {print $2}' second.txt)
+    # OBJ_UUID=$(awk 'END{print $1}' second.txt)
+    # OBJ_UUID=$(awk 'END{if (/^[0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12}/) print $1}' second.txt) better
+    # OBJ_UUID=$(awk 'END{if (/([a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}){1}/) print $1}' second.txt) wount work
+    LockFile=~/file_flow.lock
+    echo "$(tail -1000 $LOGFILENAME)" > $LOGFILENAME
+    if [ -f ${LockFile} ]; then
+        echo  "LockFile detected! FileFlow is already runing" >> $LOGFILENAME
+        exit 1
+    else
+        touch ${LockFile}
+        for file in "$SOURCEDIR"/*; do
+            FILEFLOWPATH="$SOURCEDIR"/"$file"
+            _mv $FILEFLOWPATH
+        done
+        rm -rf ${LockFile}
+    fi
+    
 }
 
 #===  END OF FUNCTIONS  ========================================================
